d4f4df398a01441dfff6d8c3c2d3d8849183d487
RaZe <razeslothc@gmail.com>
2022-10-11 19:32:15 +0300

    fix

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@ git reset --hard
 git pull
 yarn install --production
 yarn remove puppeteer
-yarn add puppeteer-core
+yarn add puppeteer-core@v1.11.0
 # Use sed to replace the executablePath in the puppeteer-core package
 sed -i "s/puppeteer/puppeteer-core/g" src/util/tunniplaan.ts && sed -i "s_({ headless: true });_({ headless: true, executablePath: '/usr/bin/chromium-browser' });_g" src/util/tunniplaan.ts
 pm2 restart 'voco-bot'
