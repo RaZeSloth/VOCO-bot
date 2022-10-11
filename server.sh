121a4907db7e16dc8022e6834b9292c8d0549d80
RaZe <razeslothc@gmail.com>
2022-10-11 19:36:41 +0300

    Moved a line

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 git reset --hard
 git pull
-yarn install --production
 yarn remove puppeteer
+yarn install --production
 yarn add puppeteer-core@v1.11.0
 # Use sed to replace the executablePath in the puppeteer-core package
 sed -i "s/puppeteer/puppeteer-core/g" src/util/tunniplaan.ts && sed -i "s_({ headless: true });_({ headless: true, executablePath: '/usr/bin/chromium-browser' });_g" src/util/tunniplaan.ts
